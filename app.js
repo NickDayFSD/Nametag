43de93c328dd71855b3dcd17df02a198fce0d1c3
NickDayFSD <80706339+NickDayFSD@users.noreply.github.com>
2021-03-18 10:48:35 -0700

    styled input and submit button

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // define variables and get elements from html
 const userName = document.getElementById('user-name');
-console.log(userName);
+
 const defaultName = document.getElementById('name-placeholder');
-console.log(defaultName);
+
 const nameButton = document.getElementById('name-button');
-console.log(nameButton);
+
 
 // create function with event listener to click
 nameButton.addEventListener('click', () => {
