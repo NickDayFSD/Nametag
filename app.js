fe26bfd38dcf601807611b22ad97b8e1acc1209e
NickDayFSD <80706339+NickDayFSD@users.noreply.github.com>
2021-03-18 15:12:42 -0700

    added color button functionality

--- app.js
+++ app.js
@@ -10,3 +10,28 @@ const nameButton = document.getElementById('name-button');
 nameButton.addEventListener('click', () => {
     defaultName.textContent = userName.value;
 })
+
+// define variables for color buttons and color
+const blueButton = document.getElementById('blue-button');
+const greenButton = document.getElementById('green-button');
+const redButton = document.getElementById('red-button');
+console.log(blueButton);
+// define variable for nametag color
+const topNametag = document.getElementById('top-nametag');
+const botNametag = document.getElementById('bot-nametag');
+
+// create functions for buttons
+redButton.addEventListener("click", () => {
+    topNametag.style.backgroundColor  = 'red';
+    botNametag.style.backgroundColor  = 'red';
+})
+
+blueButton.addEventListener("click", () => {
+    topNametag.style.backgroundColor  = 'blue';
+    botNametag.style.backgroundColor  = 'blue';
+})
+
+greenButton.addEventListener("click", () => {
+    topNametag.style.backgroundColor  = 'green';
+    botNametag.style.backgroundColor  = 'green';
+})
